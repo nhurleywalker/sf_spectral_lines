cfc85603c816edbf73677f9f54334c684e43a0cc
Dr Natasha Hurley-Walker <nhw@icrar.org>
2020-04-29 13:20:59 +0800

    New source-finding settings aiming for detections rather than noise characterisation

--- aegean.sh
+++ aegean.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-file=Vela_I_1932_Subtracted.fits
+file=Vela_I_910_Cotninuum_flagged_subtracted.fits
 
-../rms.py $file
+../sf_spectral_lines/rms.py $file
 
-zero.py ${file%.fits}_rms.fits ${file%.fits}_bkg.fits
+../sf_spectral_lines/zero.py ${file%.fits}_rms.fits ${file%.fits}_bkg.fits
 
 for i in {01..99}
 do
-    echo ${file} ${i}
-    python ./make_scaled_rms.py ${file} ${i} ${file%.fits}_rms.fits
-    aegean --cores=6 --negative --slice=${i} --seedclip=4.5 --floodclip=3.5 --table=${file%.fits}_${i}.fits --background ${file%.fits}_bkg.fits --noise ${file%.fits}_${i}_rms.fits ${file}
-    if [[ -e ${file%.fits}_${i}_comp.fits ]]
+    if [[ ! -e ${file%.fits}_${i}_comp.fits ]]
     then
-        n=`fitshdr ${file%.fits}_${i}_comp.fits | grep NAXIS2 | awk '{print $3}'`
-    else
-        n=0
+        aegean --cores=6 --negative --slice=${i} --seedclip=7 --floodclip=4 --table=${file%.fits}_${i}.fits --background ${file%.fits}_bkg.fits --noise ${file%.fits}_rms.fits ${file}
+        if [[ -e ${file%.fits}_${i}_comp.fits ]]
+        then
+            n=`fitshdr ${file%.fits}_${i}_comp.fits | grep NAXIS2 | awk '{print $3}'`
+        else
+            n=0
+        fi
+        echo "$i $n" >> detections.txt
     fi
-    echo "$i $n" >> detections.txt
 done
 
-../make_tcat.sh
+../sf_spectral_lines/make_tcat.sh
 chmod +x tcat.sh
 ./tcat.sh
-
-
